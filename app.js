754071dcacdeae8e9b1a997f15e18f6e3c38d52d
Veerendra Raghuwanshi <68636328+veer-raghuwanshi@users.noreply.github.com>
2023-08-26 12:29:19 +0530

    Update app.js

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(
 let cors = require("cors");
 // app.use(cors());
 app.use(cors({
-  // origin: 'http://localhost:3001',
+  origin: 'http://localhost:3001',
   credentials: true // Allow credentials
 }));
 app.use((req, res, next) => {
